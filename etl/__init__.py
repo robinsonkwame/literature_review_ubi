3aba275b79a52631a5deec6f5606612b14155ca7
robinsonkwame <kporterrobinson@gmail.com>
2018-03-14 16:50:21 -0400

    WIP: adds verified major topic offset and name. Was going to add sub topics but I don't think they matter for this analysis since their arguments, context, framing should be naturally drawn out

---
+++ etl/__init__.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+class ExtractTransformEconSecurityProject(object):
+    def __init__(self,
+                 data_file_path=None,
+                 major_topic_list=None):
+        self.data_file_path = data_file_path or\
+                "./data/economic_security_project/reading_list.json"
+
+        self.df = pd.read_json(self.data_file_path)
+        self.topic_list = major_topic_list or\
+                            [{"Books":7},
+                             {"Overview of UBI":20},
+                             {"Past Programs, Pilots and Findings":37},
+                             {"Current & Pending Pilots and Programs":59},
+                             {"Policy Variants and Alternatives":76},
+                             {"Political & Policy Change Strategies":91},
+                             {"Arguments for UBI":113},
+                             {"Critiques and Concerns":128},
+                             {"Misc Videos":133}]
+
+    def transform(self):
+        pass
